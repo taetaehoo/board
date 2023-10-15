c49004a8ce21beecf470e0731bf31d2fb97de8d2
daeun <eowlfhd1@daum.net>
2023-10-16 01:54:17 +0900

    다은

--- src/components/Question.js
+++ src/components/Question.js
@@ -2,7 +2,6 @@ import React from 'react'
 import styles from '../styles/Question.module.css'
 
 function Question({qnaId, title, writer, dated, hit, numOfComments}) {
-    console.log(qnaId)
   return (
     <li key={qnaId} className={styles.li}>
       <div className={styles.title}>{title} {numOfComments !== 0 ? `[${numOfComments}]` : ''}</div>
